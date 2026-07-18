c098621c67b361594e36deb9c105156688bcc2e5
Exequiel <trekan@gmail.com>
2026-07-18 13:18:27 -0400

    Se avanza hasta que falta ajustar el contenido de la sidebar y los tooltips

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,14 @@
 //   JavaScript para activar el movimiento
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.getElementById('sidebarToggle');
+    const botonHomeLink = document.getElementById('boton-home-link');
+    const homeLink = document.getElementById('home-link');
     const sidebar = document.getElementById('sidebar');
 
-    toggleBtn.addEventListener('click', function () {
+    botonHomeLink.addEventListener('click', function () {
+        // Alterna la clase que empuja el sidebar hacia la izquierda
+        sidebar.classList.toggle('sidebar-hidden');
+    });
+    homeLink.addEventListener('click', function () {
         // Alterna la clase que empuja el sidebar hacia la izquierda
         sidebar.classList.toggle('sidebar-hidden');
     });
